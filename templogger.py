7e6014c346d641f045dffa6a6aa5eb763f3cf95c
mathieuduperre <mduperre@cisco.com>
2017-04-25 14:13:07 -0400

    first load
    templogger.py log a ds18b20 sensor temperature and send the result to a thingspeak channel
    waterlogger.py log 2 float sensor (simple float sensor connected to 2 gpio ) and send 0 or 1 based on whather they are on or off. 
    
    those scripts can be called by a cron every few minutes and we'll add smartness to them later.

---
+++ templogger.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+############################################
+#           pool monitoring
+#  Read data from a DS18B20 sensor 
+#  and send to Thingspeak.com
+#
+# Author : Mathieu Duperre
+# Date   : 25/04/2017
+############################################
+
+import time
+import os
+import sys
+import urllib            # URL functions
+import urllib2           # URL functions
+
+################# Default Constants #################
+# These can be changed if required
+THINGSPEAKKEY = 'XXXXXXXXXXX'
+THINGSPEAKURL = 'https://api.thingspeak.com/update'
+tmp_sensor_name = '28-000003473299'
+base_dir = '/sys/bus/w1/devices/'
+#####################################################
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+def sendData(url,key,field1,temp):
+  """
+  Send event to internet site
+  """
+  print("sending data")
+  temp=str(temp)
+
+  values = {'key' : key,'field1' : temp}
+
+  postdata = urllib.urlencode(values)
+  req = urllib2.Request(url, postdata)
+
+  log = time.strftime("%d-%m-%Y,%H:%M:%S") + ","
+  #log = log + "{:.1f}C".format(temp) + ","
+  #log = log + "{:.2f}mBar".format(level) + ","
+
+  try:
+    # Send data to Thingspeak
+    response = urllib2.urlopen(req, None, 5)
+    html_string = response.read()
+    response.close()
+    log = log + 'Update ' + html_string
+
+  except urllib2.HTTPError, e:
+    log = log + 'Server could not fulfill the request. Error code: ' + str(e.code)
+  except urllib2.URLError, e:
+    log = log + 'Failed to reach server. Reason: ' + str(e.reason)
+  except:
+    log = log + 'Unknown error'
+
+  print log
+
+
+def read_temp_raw():
+    global device_file
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        #return temp_c, temp_f
+        return temp_f
+
+
+device_folder = base_dir + tmp_sensor_name
+device_file = device_folder + '/w1_slave'
+temperature=read_temp()
+print("temp: ",temperature)
+sendData(THINGSPEAKURL,THINGSPEAKKEY,'field1',temperature)
+sys.stdout.flush()
---
+++ waterlogger.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+############################################
+#           pool monitoring
+#  read a float sensor and send to Thingspeak.com
+#
+# Author : Mathieu Duperre
+# Date   : 25/04/2017
+############################################
+
+import time
+import RPi.GPIO as GPIO
+#import os
+import sys
+import urllib            # URL functions
+import urllib2           # URL functions
+
+################# Default Constants #################
+# These can be changed if required
+THINGSPEAKKEY = 'XXXXXXXXXX'
+THINGSPEAKURL = 'https://api.thingspeak.com/update'
+lowsensor_gpio = 27
+highsensor_gpio = 15
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(lowsensor_gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(highsensor_gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#####################################################
+
+def sendData(url,key,field2,field3,lowsensor,highsensor):
+  """
+  Send event to internet site
+  """
+  print("sending data")
+  lowsensor=str(lowsensor)
+  highsensor=str(highsensor)
+
+  values = {'key' : key,'field2' : lowsensor,'field3' : highsensor}
+
+  postdata = urllib.urlencode(values)
+  req = urllib2.Request(url, postdata)
+
+  log = time.strftime("%d-%m-%Y,%H:%M:%S") + ","
+
+  try:
+    # Send data to Thingspeak
+    response = urllib2.urlopen(req, None, 5)
+    html_string = response.read()
+    response.close()
+    log = log + 'Update ' + html_string
+
+  except urllib2.HTTPError, e:
+    log = log + 'Server could not fulfill the request. Error code: ' + str(e.code)
+  except urllib2.URLError, e:
+    log = log + 'Failed to reach server. Reason: ' + str(e.reason)
+  except:
+    log = log + 'Unknown error'
+
+  print log
+
+
+def read_low_sensor():
+    input_state = GPIO.input(lowsensor_gpio)
+    if input_state == False:
+        return 0
+    else:
+        return 1
+
+
+def read_high_sensor():
+    input_state = GPIO.input(highsensor_gpio)
+    if input_state == False:
+        return 0
+    else:
+        return 1
+
+
+low_floater_state=read_low_sensor()
+high_floater_state=read_high_sensor()
+print("Low Sensor State: ",low_floater_state)
+print("High Sensor State: ",high_floater_state)
+sendData(THINGSPEAKURL,THINGSPEAKKEY,'field2','field3',low_floater_state,high_floater_state)
+sys.stdout.flush()
